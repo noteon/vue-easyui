894b519394d62938b882adc8f4dc7a367335b3a6
qinghai <noteon@gmail.com>
2016-05-06 16:10:25 +0800

    setup env

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
+declare var $ :any;
 import Component from 'vue-class-component'
 
 @Component({
@@ -40,6 +41,30 @@ class App {
 
 // mount
 let app=new (App as any)({
+    el:'#el'
 })
 
-app.$mount('#el');
+$(()=>{
+    $("#easyui-combo").combobox({
+        idField:"id",
+        textField:"text",
+        data:[{
+            "id":1,
+            "text":"text1"
+        },{
+            "id":2,
+            "text":"text2"
+        },{
+            "id":3,
+            "text":"text3",
+            "selected":true
+        },{
+            "id":4,
+            "text":"text4"
+        },{
+            "id":5,
+            "text":"text5"
+        }]
+});
+
+})
